0f9ae99a27594641034f236e24a6ac2a9efa2b10
francis-ux <kingmicheal744@gmail.com>
2024-12-13 21:27:35 +0100

    basic loops

---
+++ 02-arrays-and-iteration/05-basic-loops/index.php
@@ -0,0 +1,23 @@
+<?php
+// Loops in php
+// 1 - while loop
+// 2 - for loop
+// 3 - foreach loop
+
+// For loop
+// for ($i = 0; $i <= 10; $i++) {
+//     echo $i . "\n";
+// }
+
+// While loop
+// $i = 0;
+// while ($i <= 10) {
+//     echo $i . "\n";
+//     $i++;
+// }
+
+// Foreach loop
+$fruits = ['apple', 'banana', 'orange'];
+foreach ($fruits as $fruit) {
+    echo $fruit . "\n";
+}
