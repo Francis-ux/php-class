11c6ef4a8b29f9776fa8012ea74162ad41faeb30
francis-ux <kingmicheal744@gmail.com>
2024-12-17 21:20:25 +0100

    logical and comparison operators

---
+++ 03-control-structures/02-comparison-logical-operators/index.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Comparison Operators
+
+== equal to
+!= not equal to
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+=== Identical To
+!== Not Identical To
+<> not equal to
+*/
+
+/*
+Logical Operators
+
+and - both conditions must be true
+or - at least one condition must be true
+&& - both conditions must be true
+|| - at least one condition must be true
+xor - exactly one condition must be true
+! - reverse the condition - True if it is not true
+*/
